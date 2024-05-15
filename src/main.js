0d97c06aa510ed83ae1cb03376696ff966493a7d
czz <1006098429@qq.com>
2024-05-15 20:12:32 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+// 导入路由规则
+import router from './router'
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
